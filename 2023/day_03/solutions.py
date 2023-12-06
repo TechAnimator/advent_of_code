0215e756446eb4560bd59fc11037cc18a7649ab3
Brian Venisky <briven@users.noreply.github.com>
2023-12-05 22:27:27 -0500

    Update solutions.py
    Part 1 solved

--- 2023/day_03/solutions.py
+++ 2023/day_03/solutions.py
@@ -20,19 +20,49 @@ def do_it(input):
     Returns:
 
     """
-    for line in input:
-        if line == input[1]:
-            numbers_pattern = re.finditer(r'\d+', line) # Find all instances of numbers in the line
-            symbols_pattern = re.finditer(r'[^.|^\d]+', line) # Find anything that isn't a number or a period in the line
-            
-            number_key = dict()
-            for idx, number in enumerate(numbers_pattern):
-                number_key[idx] = [number.start() - 1, number.end() + 1]
-            print(number_key)
-            print()
-            for symbol in symbols_pattern:
-                print(symbol.group(), symbol.start())
-            print()
+    stored_data = dict()
+    for line_idx, line in enumerate(input):
+        numbers_pattern = re.finditer(r'\d+', line) # Find all instances of numbers in the line
+        symbols_pattern = re.finditer(r'[^.|^\d]+', line) # Find anything that isn't a number or a period in the line
+
+        number_key = dict()
+        symbol_locations = list()
+        for idx, number in enumerate(numbers_pattern):
+            # Store every instance of a number in it's own embedded dict with the number and the range it appears.
+            # Expand the range by 1 on each side to account for diagonal symbols when comparing lines.
+            number_key[idx] = {"number": int(number.group()), "range": [number.start() - 1, number.end() + 1]}
+
+        for symbol in symbols_pattern:
+            symbol_locations.append(symbol.start())
+
+        stored_data[line_idx] = {"numbers": number_key, "symbols": symbol_locations}
+
+    data_length = len(stored_data)
+    total = 0
+    for line_number in range(data_length):
+        if line_number != 0:
+            last_line = stored_data[line_number-1]
+        else:
+            last_line = {"symbols": list()}
+
+        if line_number != data_length-1:
+            next_line = stored_data[line_number+1]
+        else:
+            next_line = {"symbols": list()}
+        current_line = stored_data[line_number]
+
+        all_symbols = list()
+        all_symbols.extend(last_line["symbols"])
+        all_symbols.extend(current_line["symbols"])
+        all_symbols.extend(next_line["symbols"])
+        all_symbols.sort()
+
+        for idx, data in current_line["numbers"].items():
+            next_to_symbol = any(data["range"][0] <= x < data["range"][1] for x in all_symbols)
+            if next_to_symbol:
+                total += data["number"]
+
+    return total
 
 
 if __name__ == "__main__":
