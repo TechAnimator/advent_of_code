df0b596c20150646bd1413f819173b6de2ba1a5e
Brian Venisky <4052825+TechAnimator@users.noreply.github.com>
2023-12-06 16:20:59 -0500

    Update solutions.py
    Starting part 2

--- 2023/day_03/solutions.py
+++ 2023/day_03/solutions.py
@@ -30,6 +30,7 @@ def do_it(input):
 
         number_key = dict()
         symbol_locations = list()
+        gear_locations = list()
         for idx, number in enumerate(numbers_pattern):
             # Store every instance of a number in its own unique embedded dict with the number and the range it appears.
             # Expand the range by 1 on each side to account for diagonal symbols when comparing lines.
@@ -38,36 +39,50 @@ def do_it(input):
         # Store the indicies for all symbols (we don't care what the symbols are, just where they are)
         for symbol in symbols_pattern:
             symbol_locations.append(symbol.start())
+            if symbol.group() == "*": # For part 2, if a symbol is a gear -> *, store that location in an additional list
+                gear_locations.append(symbol.start())
 
         # Store the number dict and symbol list for the line
-        stored_data[line_idx] = {"numbers": number_key, "symbols": symbol_locations}
+        stored_data[line_idx] = {"numbers": number_key, "symbols": symbol_locations, "gears": gear_locations}
 
     # Get the number of lines to loop through and create the total variable to add up the numbers applicable to the puzzle prompt
     data_length = len(stored_data)
-    total = 0
+    part_one_total = 0
+    part_two_total = 0
 
     for line_number in range(data_length):
         all_symbols = list()
+        all_gears = list()
 
         if line_number != 0: # Add the symbol locations from the previous line for the 2nd line and beyond
             all_symbols.extend(stored_data[line_number-1]["symbols"])
+            all_gears.extend(stored_data[line_number-1]["gears"])
         if line_number != data_length - 1: # Add the symbol locations from the next line up until the last line
             all_symbols.extend(stored_data[line_number+1]["symbols"])
+            all_gears.extend(stored_data[line_number+1]["gears"])
         all_symbols.extend(stored_data[line_number]["symbols"]) # Add the symbols locations from the current line
+        all_gears.extend(stored_data[line_number]["gears"]) # Add the symbols locations from the current line
 
         all_symbols.sort() # Unnecessary but I like a sorted list of numbers
+        all_gears.sort()
 
+        gear_data = dict()
+        
         # Go through all the numbers in the current line and cross check with the symbols from the previous, current, and next
         # lines to see what numbers are applicable to the puzzle prompt and next to a symbol
         for idx, data in stored_data[line_number]["numbers"].items():
-            next_to_symbol = any(data["range"][0] <= x < data["range"][1] for x in all_symbols)
+            next_to_symbol = any(data["range"][0] <= symbol < data["range"][1] for symbol in all_symbols)
             if next_to_symbol:
-                total += data["number"]
+                part_one_total += data["number"]
 
-    return total
+        for gear in all_gears:
+            print(line_number, gear)
+
+    return part_one_total, part_two_total
 
 
 if __name__ == "__main__":
     input = get_input()
-    print("Part 1:", do_it(input))
-    # print("Part 2:", do_it(input))
+    part_one, part_two = do_it(input)
+    print("Part 1:", part_one)
+    print("Part 2:", part_two)
