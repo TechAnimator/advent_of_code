60da9c22738bce4c7c879e4c406cca8cf49c8d00
Brian Venisky <4052825+TechAnimator@users.noreply.github.com>
2024-01-05 12:54:15 -0500

    Update solutions.py
    Parts one and two both solved.

--- 2023/day_07/solutions.py
+++ 2023/day_07/solutions.py
@@ -28,24 +28,47 @@ def do_it(input, part_two=False):
         "Q": 12,
         "J": 11,
         "T": 10,
-        "X": 1,
     }
 
     all_hands = []
 
     for line in input:
-        hand, bid = line.split(" ")
-        hand = list(hand)
-        for idx, card in enumerate(hand):
+        cards_hand, bid = line.split(" ")
+        cards_hand = list(cards_hand)
 
+        
+        values_hand = cards_hand.copy()
+        for idx, card in enumerate(values_hand):
             card_value = int(card_values.get(card, card))
-            hand[idx] = card_value
-    
-        card_counter = Counter(hand)
-        card_counter = Counter(hand).most_common(5)
+            values_hand[idx] = card_value
+
+        card_counter = Counter(values_hand)
+        
+        wild_card = None
+        wild_hand = None
+        if part_two:
+            if 11 in card_counter:
+                checker = card_counter.copy()
+                del checker[11]
+                
+                wild_cards = Counter(checker).most_common(2)
+
+                if len(wild_cards) > 1 and wild_cards[0][1] == wild_cards[1][1]:
+                    wild_card = max(wild_cards[0][0], wild_cards[1][0])
+                elif wild_cards:
+                    wild_card = wild_cards[0][0]
+
+                wild_hand = [wild_card if original_value == 11 else original_value for original_value in values_hand]
+                values_hand = [1 if original_value == 11 else original_value for original_value in values_hand]
+
+        if not wild_card:
+            card_counter = Counter(values_hand).most_common(5)
+        else:
+            card_counter = Counter(wild_hand).most_common(5)
+
         card_counter = [count for _, count in card_counter]
 
-        hand_bid_pair = [card_counter, hand, int(bid)]
+        hand_bid_pair = [card_counter, values_hand, int(bid)]
         all_hands.append(hand_bid_pair)
         
     all_hands.sort(key = operator.itemgetter(0, 1))
