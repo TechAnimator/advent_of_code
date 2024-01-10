b1b560bd4049ffa1c082fcad7c4507d9ad96a96a
Brian Venisky <4052825+TechAnimator@users.noreply.github.com>
2024-01-10 14:11:38 -0500

    Update solutions.py
    Cleaned up code and added documentation

--- 2023/day_08/solutions.py
+++ 2023/day_08/solutions.py
@@ -11,74 +11,66 @@ def get_input():
     return [line for line in Path(__file__).parent.joinpath("input.txt").read_text().split("\n")]
 
 
-def do_it(input):
-    """Method for both parts one and two. Part one solves the puzzle using number values as given, part two accounts for Jacks as Jokers worth 1, not 11.
+def do_it(input, part_two=False):
+    """Method for both parts one and two. The difference is searching for the element ZZZ vs elements all ending in Z and going through 1 node at a time
+    vs simultaneously going through multiple nodes.
 
     Args:
         input (list): Input for the puzzle sorted as lines (str) in a list.
+        part_two (bool): True if solving part two. False if solving part one.
 
     Returns:
         int: Number of steps to get from AAA to ZZZ
     """
     steps = dict()
     for line in input:
+        # Store elements in a dict, key is the element itself and value is 2 elements for left/right based on instruction
         if "=" in line:
             step, left_right = line.split(" = ")
             left_right = re.findall(r'[A-Z]+', left_right)
             left_right = [code for code in left_right]
             steps[step] = left_right
         elif line:
-            instructions = line
+            instructions = line # Left/Right instructions
 
-    return part_one(instructions, steps), part_two(instructions, steps)
+    # Part two looks for elements that end in Z while part 1 looks for the element that is exactly ZZZ
+    # Part two uses multiple elements while part 1 only starts with AAA
+    if part_two:
+        key_to_search_for = "Z"
+        first_steps = [directions for step, directions in steps.items() if step.endswith("A")]
+    else:
+        key_to_search_for = "ZZZ"
+        first_steps = [steps["AAA"]]
 
-
-def part_one(instructions, steps):
-    current_step = steps["AAA"]
-
-    step_count = 0
-    not_zzz = True    
-    while not_zzz:
-        for direction in instructions:
-            if direction == "L":
-                current_key = current_step[0]
-            else:
-                current_key = current_step[1]
-            current_step = steps[current_key]
-            step_count += 1
-
-            if current_key == "ZZZ":
-                not_zzz = False
-
-    return step_count
-
-
-def part_two(instructions, steps):
-    first_steps = [directions for step, directions in steps.items() if step.endswith("A")]
-
+    # Store the number of steps necessary to find the end key (elements ending in Z for part 2, or ZZZ for part 1)
     steps_to_z = []
-    for step in first_steps:
+    for current_left_right in first_steps:
         step_count = 0
-        not_zzz = True  
-        while not_zzz:
+        element_is_not_goal = True # Set this to true since the first element isn't our goal
+        while element_is_not_goal: # Keep going until the element we need is found
+            # Go through the left/right instructions, counting steps for each iteration
             for direction in instructions:
                 if direction == "L":
-                    current_key = step[0]
+                    current_key = current_left_right[0]
                 else:
-                    current_key = step[1]
-                step = steps[current_key]
+                    current_key = current_left_right[1]
+                current_left_right = steps[current_key]
                 step_count += 1
-            if current_key.endswith("Z"):
-                not_zzz = False
-        steps_to_z.append(step_count)
 
-    return math.lcm(*steps_to_z)
+                # When the element that is equal to Z (part 1) or ending in Z (part 2) is found, break the loop and set the bool to False
+                if current_key.endswith(key_to_search_for):
+                    element_is_not_goal = False
+
+        steps_to_z.append(step_count) # Add the step count to the list we will find the lowest common multiple from
+
+    return math.lcm(*steps_to_z) # Get the lowest common multiple from all the step counts (part 1 will just have a single step count)
     
 
 if __name__ == "__main__":
     input = get_input()
 
-    part_one, part_two = do_it(input)
+    part_one = do_it(input)
+    part_two = do_it(input, part_two=True)
 
     print("\nPart 1:", part_one)
     print("\nPart 2:", part_two)
