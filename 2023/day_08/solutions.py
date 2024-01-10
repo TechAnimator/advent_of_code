800dae299c950a8ad73b46bfa1bc6bfc4a08cbfe
Brian Venisky <4052825+TechAnimator@users.noreply.github.com>
2024-01-10 10:50:58 -0500

    Update solutions.py
    Part Two done

--- 2023/day_08/solutions.py
+++ 2023/day_08/solutions.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import re
-
+import math
 
 def get_input():
     """Parse the input.txt file in the current directory and return lines as a list.
@@ -11,7 +11,7 @@ def get_input():
     return [line for line in Path(__file__).parent.joinpath("input.txt").read_text().split("\n")]
 
 
-def do_it(input, part_two=False):
+def do_it(input):
     """Method for both parts one and two. Part one solves the puzzle using number values as given, part two accounts for Jacks as Jokers worth 1, not 11.
 
     Args:
@@ -20,8 +20,6 @@ def do_it(input, part_two=False):
     Returns:
         int: Number of steps to get from AAA to ZZZ
     """
-    number_of_steps = None
-
     steps = dict()
     for line in input:
         if "=" in line:
@@ -31,7 +29,11 @@ def do_it(input, part_two=False):
             steps[step] = left_right
         elif line:
             instructions = line
-    
+
+    return part_one(instructions, steps), part_two(instructions, steps)
+
+
+def part_one(instructions, steps):
     current_step = steps["AAA"]
 
     step_count = 0
@@ -47,16 +49,36 @@ def do_it(input, part_two=False):
 
             if current_key == "ZZZ":
                 not_zzz = False
-                break
-        
+
     return step_count
 
+
+def part_two(instructions, steps):
+    first_steps = [directions for step, directions in steps.items() if step.endswith("A")]
+
+    steps_to_z = []
+    for step in first_steps:
+        step_count = 0
+        not_zzz = True  
+        while not_zzz:
+            for direction in instructions:
+                if direction == "L":
+                    current_key = step[0]
+                else:
+                    current_key = step[1]
+                step = steps[current_key]
+                step_count += 1
+            if current_key.endswith("Z"):
+                not_zzz = False
+        steps_to_z.append(step_count)
+
+    return math.lcm(*steps_to_z)
+    
 
 if __name__ == "__main__":
     input = get_input()
 
-    part_one = do_it(input)
-    # part_two = do_it(input, part_two=True)
+    part_one, part_two = do_it(input)
 
     print("\nPart 1:", part_one)
-    # print("\nPart 2:", part_two)
+    print("\nPart 2:", part_two)
